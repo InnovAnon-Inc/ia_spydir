535b6910d5799df3dd5695ef866ef5d759ab75ca
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2024-11-27 11:39:42 -0600

    Initial Commit

--- ia_spydir/main.pyx
+++ ia_spydir/main.pyx
@@ -354,7 +354,6 @@ class SPyDirConfig():
 
 	def update_index(self,)->None:
 		docs:List[Document] = self.load_data()
-		logger.debug('#docs: %s', len(docs),)
 		self.index.refresh(docs)
 
 #async def communicate(client:AsyncClient, url:str, message:str, uid:str,)->str:
